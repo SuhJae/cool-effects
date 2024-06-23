8a0f03ee9ebf88dde654aacb5a673a51b565de14
suhjae <jwsuh25@kis.ac>
2024-06-23 10:54:59 +0900

    feat: parallaxEffect demo

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
 <template>
   <NuxtPage />
 </template>
+
+<style>
+.titleBase {
+  @apply text-white text-center space-y-4 bg-black w-full h-full flex flex-col justify-center items-center bg-opacity-10 opacity-90 drop-shadow-lg;
+}
+</style>
--- pages/parallax.vue
+++ pages/parallax.vue
@@ -17,7 +17,5 @@
 </template>
 
 <style scoped>
-.titleBase {
-  @apply text-white text-center space-y-4 bg-black w-full h-full flex flex-col justify-center items-center bg-opacity-10 opacity-90 drop-shadow-lg;
-}
+/*  */
 </style>
